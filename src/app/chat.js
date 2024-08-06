5c81501a6dfe461d8ca76b68342d4985e1911e97
Nathan Gilman <ngilman@ufl.edu>
2024-08-06 14:13:36 -0400

    Commenting out code until we can get backend or a way to manage chatgpt

--- src/app/chat.js
+++ src/app/chat.js
@@ -1,16 +1,17 @@
 import OpenAI from "openai"; 
 
-const openai = new OpenAI();
+// const openai = new OpenAI();
 
 const aiResponse = async (message) => {
-    console.log(message);
-    const chatCompletion = await openai.chat.completions.create({
-        messages: [{
-            role: "user", content: message
-        }],
-        model: "gpt-3.5-turbo"
-    })
-    return chatCompletion.choices[0].message.content;
+    // console.log(message);
+    // const chatCompletion = await openai.chat.completions.create({
+    //     messages: [{
+    //         role: "user", content: message
+    //     }],
+    //     model: "gpt-3.5-turbo"
+    // })
+    // return chatCompletion.choices[0].message.content;
+    return "placeholder";
 };
 
 export default aiResponse;
